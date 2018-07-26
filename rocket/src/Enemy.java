d3ad2e8d5e16a350136404494a5db646fd9125c5
Hiep Nguyen <dr.hiep98@gmail.com>
2018-07-27 01:49:24 +0700

    bai 1234

---
+++ rocket/src/Enemy.java
@@ -0,0 +1,54 @@
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.util.Random;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Hiep Nguyen
+ */
+public class Enemy {
+
+    public GameCanvas gameCanvas = new GameCanvas();
+    public Player player = new Player();
+    public int vx, vy;
+    public BufferedImage image;
+    public int x, y;
+    public int w, h;
+    public int dx ,dy;
+    public Random rd = new Random();
+
+    public void render(Graphics g) {
+        g.setColor(Color.yellow);
+        g.fillOval(x, y, w, h);
+    }
+
+    public void run() {
+        
+        dx = gameCanvas.player.x[0] - x;
+        dy = gameCanvas.player.y[0] - y;
+        
+     
+        float distance =(float) Math.sqrt((dx*dx) + (dy*dy));
+        
+        
+        
+        vx = (int) ((int) (2*dx)/distance);
+        vy = (int) ((int) (2*dy)/distance);
+
+        x += vx;
+        y += vy;
+//        if (this.x < 0 || this.x > 1000) {
+//            vx = -vx;
+//        }
+//        if (this.y < 0 || this.y > 542) {
+//            vy = -vy;
+//        }
+    }
+}
---
+++ rocket/src/GameWindow.java
@@ -0,0 +1,95 @@
+
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class GameWindow extends JFrame {
+
+   static  GameCanvas gameCanvas=new GameCanvas();
+   static  Player player=new Player();
+    private long lastTime = 0;
+
+    public void event() {
+        windowEven();
+        keybroadEvent();
+
+    }
+
+    private void windowEven() {
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void keybroadEvent() {
+        addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    gameCanvas.player.x[1] -= 10;
+                    gameCanvas.player.x[0] -= 10;
+                    gameCanvas.player.x[2] -= 10;
+                //    System.out.println(" x[0] " + gameCanvas.player.x[0] + " y[0] " + gameCanvas.player.y[0] + "\n" + " x[1] " + gameCanvas.player.x[1] + " y[1] " + gameCanvas.player.y[1] + "\n" + " x[2] " + gameCanvas.player.x[2] + " y[2] " + gameCanvas.player.y[2]);
+                }
+                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    gameCanvas.player.x[1] += 10;
+                    gameCanvas.player.x[0] += 10;
+                    gameCanvas.player.x[2] += 10;
+                 //    System.out.println(" x[0] " + gameCanvas.player.x[0] + " y[0] " + gameCanvas.player.y[0] + "\n" + " x[1] " + gameCanvas.player.x[1] + " y[1] " + gameCanvas.player.y[1] + "\n" + " x[2] " + gameCanvas.player.x[2] + " y[2] " + gameCanvas.player.y[2]);
+                }
+                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    gameCanvas.player.y[1] += 10;
+                    gameCanvas.player.y[0] += 10;
+                    gameCanvas.player.y[2] += 10;
+                }
+                if (e.getKeyCode() == KeyEvent.VK_UP) {
+
+                    gameCanvas.player.y[1] -= 10;
+                    gameCanvas.player.y[0] -= 10;
+                    gameCanvas.player.y[2] -= 10;
+
+                }
+
+            }
+
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+
+    }
+
+    private void setupGamecanvas() {
+        gameCanvas = new GameCanvas();
+        add(gameCanvas);
+    }
+
+    public GameWindow() {
+
+        setSize(1024, 600);
+        setupGamecanvas();
+        this.event();
+
+        this.setVisible(true);
+
+    }
+
+    public void gameLoop() {
+        while (true) {
+            long currentTime = System.nanoTime();
+            if (currentTime - this.lastTime >= 17_000_000) {
+                this.gameCanvas.renderAll();
+                this.lastTime = currentTime;
+                gameCanvas.runAll();
+
+            }
+
+        }
+    }
+}
